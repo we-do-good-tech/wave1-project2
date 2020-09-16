c76a275dcff6fafead072152d50298361a559547
ruslanNir <nirkuba19999@gmail.com>
2020-09-16 14:46:19 +0300

    connect-db

---
+++ db/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+
+
+const url = "";
+
+
+async function connectMongoDB(request, response, next) {
+
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        })
+
+        console.log('Connect to database')
+
+        // next()
+
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+
+}
+
+
+module.exports = { connectMongoDB }
