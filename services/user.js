5e3b825a080554dad98a7daba3f6d87d394178b5
AdamGolan69 <slowmoon.adam@gmail.com>
2020-09-20 11:12:35 +0300

    update

--- services/user.js
+++ services/user.js
@@ -1,41 +1,26 @@
 module.exports = {
     // Admining
-    addAdmin: async function (newAdmin) {
-
-    },
-    registerAdmin: async function (regAdmin) {
-
-    },
-    addTeacher: async function (newTeacher) {
-
-    },
-    updateTeacher: async function (teacher) {
-
-    },
-    deleteTeacher: async function (teacher) {
-
-    },
-    addStudent: async function (newStudent) {
-
-    },
-    updateStudent: async function (student) {
-
-    },
-    deleteStudent: async function (student) {
-
-    },
+    addAdmin: async function (newAdmin) { },
+    registerAdmin: async function (regAdmin) { },
+    addTeacher: async function (newTeacher) { },
+    updateTeacher: async function (teacher) { },
+    deleteTeacher: async function (teacher) { },
+    addStudent: async function (newStudent) { },
+    updateStudent: async function (student) { },
+    deleteStudent: async function (student) { },
     // Teaching
-    registerTeacher: async function (newTeacher) {
-
-    },
-    updateStudent: async function (student) {
-
-    },
-    addMeet: async function (newMeet) {
-
-    },
+    registerTeacher: async function (newTeacher) { },
+    updateStudent: async function (student) { },
+    addMeet: async function (teacher, student, newMeet) { },
     // Parenting
-    confirmation: async function (meet) {
-
-    }
+    confirmation: async function (meet) { },
+    // Information
+    // // Getting
+    getAllStudents: async function () { },
+    getAllTeachers: async function () { },
+    getMyStudents: async function (teacher) { },
+    getStudentMeetings: async function (teacher, student) { },
+    // // Searching
+    searchTeacher: async function (inp) { },
+    searchStudent: async function (inp) { },
 }
