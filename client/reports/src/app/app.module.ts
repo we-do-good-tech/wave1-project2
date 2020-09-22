9df2644b87bae7ae4c27b563b7b730aa46c4c202
AdamGolan69 <slowmoon.adam@gmail.com>
2020-09-22 14:29:14 +0300

    update

--- client/reports/src/app/app.module.ts
+++ client/reports/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthRoutingModule } from './auth/auth.routing.module';
-import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
   ],
   imports: [
     BrowserModule,
