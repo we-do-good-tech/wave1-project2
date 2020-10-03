67af577da73cf2742b54e1bf215cc4eb88e19e75
JonnyCohen-ux <you@example.com>
2020-10-03 18:46:00 +0300

    meetings table C  created

--- client/src/app/teacher/meetings/meeting-table-item/meeting-table-item.component.ts
+++ client/src/app/teacher/meetings/meeting-table-item/meeting-table-item.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-meeting-table-item',
-  templateUrl: './meeting-table-item.component.html',
-  styleUrls: ['./meeting-table-item.component.scss']
+  selector: "app-meeting-table-item",
+  templateUrl: "./meeting-table-item.component.html",
+  styleUrls: ["./meeting-table-item.component.scss"],
 })
 export class MeetingTableItemComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
