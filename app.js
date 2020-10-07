75f7db500dc7212945a245653ac1bf2974b3b914
ruslanNir <nirkuba19999@gmail.com>
2020-10-07 22:58:55 +0300

    a

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 require("dotenv").config();
 const cors = require("cors");
-const keys = require("./back-end/config/keys");
+// const keys = require("./back-end/config/keys");
 const express = require("express");
 const path = require("path");
 const server = express();
 
 
+const authRoutes = require("./back-end/routes/auth");
 
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use(express.static(path.join(__dirname, "client/dist/reports")));
+server.use(express.static(path.join(__dirname, 'client/dist/reports')));
 
-const authRoutes = require("./back-end/routes/auth");
 
 
 server.use("/api/auth", authRoutes);
-
 
 
 server.get('*', (request, response) => {
@@ -24,4 +23,4 @@ server.get('*', (request, response) => {
 })
 
 
-server.listen(process.env.PORT || keys.port, () => console.log("Listening"));
+server.listen(process.env.PORT || 3000, () => console.log("Listening"));
