10abe5a6931bbec9372036f720e37a3342303a81
ruslanNir <nirkuba19999@gmail.com>
2020-10-11 14:50:32 +0300

    resolver-path

--- app.js
+++ app.js
@@ -9,23 +9,25 @@ const server = express();
 const authRoutes = require("./back-end/routes/auth");
 const { limiter } = require('./back-end/services/rate-limiter')
 
+const appFolder = 'client/dist/reports';
+
+
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, 'client/dist/reports')));
-
+// server.use('*.*', express.static(appFolder, { maxAge: '1y' }));
 
 
 server.use("/api/auth", limiter, authRoutes);
 
 
 
-const appFolder = 'client/dist/reports';
 
 
 server.get('*', (request, response) => {
-    // response.sendFile(path.resolve('client/dist/reports/index.html'))
-    response.status(200).sendFile(`/`, { root: appFolder });
+    response.sendFile(path.resolve('client/dist/reports/index.html'))
+    // response.status(200).sendFile(`/`, { root: appFolder + '/' + 'index.html' });
 })
 
 
