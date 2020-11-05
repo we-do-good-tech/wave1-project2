cc8c72a66ad84fd3e752e3cbd68c2910f08d03ad
ruslanNir <nirkuba19999@gmail.com>
2020-11-05 05:30:48 +0200

    h-email

--- back-end/services/emails/email.tamplates.js
+++ back-end/services/emails/email.tamplates.js
@@ -10,8 +10,8 @@ module.exports = {
         return {
             subject: 'TEST',
             text: 'TEST',
-            html: `<a href="http://localhost:3000/confirm-report/parent-signature/${token}">כאן</a> לאשר את המפש נא ללחוץ`
-            // html: `<a href="https://mashlimim.herokuapp.com/confirm-report/parent-signature/${token}">כאן</a> לאשר את המפש נא ללחוץ`
+            // html: `<a href="http://localhost:3000/confirm-report/parent-signature/${token}">כאן</a> לאשר את המפש נא ללחוץ`
+            html: `<a href="https://mashlimim.herokuapp.com/confirm-report/parent-signature/${token}">כאן</a> לאשר את המפש נא ללחוץ`
         }
     }
 }
